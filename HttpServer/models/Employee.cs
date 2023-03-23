2ac3485e9cf2fc279621a60d75d341704409e493
Nikita <nikita.dronoff@gmail.com>
2023-03-23 21:30:03 +0600

    - создал и описал класс-модель Employee - создал файл employees.json

---
+++ HttpServer/models/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace HttpServer.models;
+
+public class Employee
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string About { get; set; }
+    public int Age { get; set; }
+
+    public Employee(string name, string surname, string about, int age)
+    {
+        Id = new Guid();
+        Name = name;
+        Surname = surname;
+        About = about;
+        Age = age;
+    }
+}
