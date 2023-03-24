8dd5cef4693c10137b6b711222ec191cdb874f42
Nikita <nikita.dronoff@gmail.com>
2023-03-24 23:01:53 +0600

    - Создал и описал класс EmployeeService.cs - Создал и описал класс EmployeeViewModel.cs

---
+++ HttpServer/services/EmployeeService.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using HttpServer.dto;
+using HttpServer.managers;
+using HttpServer.models;
+using HttpServer.viewModels;
+
+namespace HttpServer.services;
+
+public class EmployeeService
+{
+    private readonly FileManager _fileManager;
+
+    public EmployeeService(FileManager fileManager)
+    {
+        _fileManager = fileManager;
+    }
+
+    public ResponseDto<List<EmployeeViewModel>> GetAll(string filePath)
+    {
+        var jsonString = _fileManager.GetContent(filePath);
+        List<Employee>? employees = JsonSerializer.Deserialize<List<Employee>>(jsonString);
+        
+        ResponseDto<List<EmployeeViewModel>> responseDto = new ResponseDto<List<EmployeeViewModel>>()
+        {
+            Result = employees is null ? new List<EmployeeViewModel>() :
+                employees.Select(x => new EmployeeViewModel
+                {
+                    About = x.About,
+                    Age = x.Age,
+                    Id = x.Id,
+                    Surname = x.Surname,
+                    Name = x.Name
+                }).ToList()
+        };
+
+        return responseDto;
+    }
+}
---
+++ HttpServer/viewModels/EmployeeViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HttpServer.viewModels;
+
+public class EmployeeViewModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string About { get; set; }
+    public int Age { get; set; }
+}
